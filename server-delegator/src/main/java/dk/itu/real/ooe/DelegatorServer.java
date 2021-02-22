11a0c2481cb4861664383969e029f1fbec10cac1
gobs <chec@itu.dk>
2021-02-22 13:27:35 +0100

    Added new server delegator project

---
+++ server-delegator/src/main/java/dk/itu/real/ooe/DelegatorServer.java
@@ -0,0 +1,10 @@
+package dk.itu.real.ooe;
+
+import java.awt.image.ImagingOpException;
+
+public class DelegatorServer {
+    public void main(String[] args) throws ImagingOpException, InterruptedException {
+        int port = 5001;
+
+    }
+}
---
+++ server-delegator/src/main/java/dk/itu/real/ooe/services/DelegatorService.java
@@ -0,0 +1,4 @@
+package dk.itu.real.ooe.services;
+
+public class DelegatorService {
+}
