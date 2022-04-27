7ef9561fc0295bb1815025116b1b62e6fa5facde
Đorđe Grbić <968662+djole@users.noreply.github.com>
2022-04-27 14:15:10 +0200

    Dev (#17)
    * Entity spawning and reading (#8)
    
    * Added methods for spawning and reading entities
    
    * updated python clients
    
    * Removed unused imports
    
    * Removed chunk loading which doesn't work
    
    * deleted unused imports
    
    Co-authored-by: Frederik Rasmussen <fgor@itu.dk>
    Co-authored-by: gobs <chec@itu.dk>
    
    * read and fill cube can find the min and max point dynamically (#12)
    
    Co-authored-by: FGOR <41154304+fred5229@users.noreply.github.com>
    Co-authored-by: Frederik Rasmussen <fgor@itu.dk>
    Co-authored-by: gobs <chec@itu.dk>

--- src/main/java/dk/itu/real/ooe/services/MinecraftService.java
+++ src/main/java/dk/itu/real/ooe/services/MinecraftService.java
@@ -1,12 +1,18 @@
 package dk.itu.real.ooe.services;
 
-
 import com.google.protobuf.Empty;
 import dk.itu.real.ooe.Minecraft;
 import dk.itu.real.ooe.Minecraft.*;
 import dk.itu.real.ooe.MinecraftServiceGrpc.MinecraftServiceImplBase;
 import io.grpc.stub.StreamObserver;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.spongepowered.api.Sponge;
+import org.spongepowered.api.entity.Entity;
+import org.spongepowered.api.entity.EntityTypes;
+import org.spongepowered.api.event.cause.EventContextKeys;
+import org.spongepowered.api.event.CauseStackManager.StackFrame;
+import org.spongepowered.api.event.cause.entity.spawn.SpawnTypes;
 import org.spongepowered.api.block.BlockState;
 import org.spongepowered.api.block.BlockType;
 import org.spongepowered.api.block.BlockTypes;
@@ -17,11 +23,14 @@ import org.spongepowered.api.scheduler.Task;
 import org.spongepowered.api.util.Direction;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
+import com.flowpowered.math.vector.Vector3d;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.ArrayList;
 
 
 public class MinecraftService extends MinecraftServiceImplBase {
@@ -29,6 +38,7 @@ public class MinecraftService extends MinecraftServiceImplBase {
 
     private final PluginContainer plugin;
     private final Map<String, String> blockNamesToBlockTypes = new HashMap<>(); // minecraft:dirt --> DIRT
+    private final Map<String, String> entityNamesToEntityTypes = new HashMap<>(); //minecraft:creeper --> CREEPER
 
 
     public MinecraftService(PluginContainer plugin) throws IllegalAccessException {
@@ -41,6 +51,12 @@ public class MinecraftService extends MinecraftServiceImplBase {
             blockNamesToBlockTypes.put(key, value);
         }
 
+        for (Field field : EntityTypes.class.getFields()) {
+            org.spongepowered.api.entity.EntityType entityType = (org.spongepowered.api.entity.EntityType) field.get(null);
+            String key = entityType.getName();
+            String value = field.getName();
+            entityNamesToEntityTypes.put(key, value);
+        }
     }
 
     @Override
@@ -68,60 +84,151 @@ public class MinecraftService extends MinecraftServiceImplBase {
     }
 
     @Override
+    public void readEntitiesInSphere(Sphere request, StreamObserver<Entities> responseObserver) {
+        Task.builder().execute(() -> {
+                Entities.Builder builder = Entities.newBuilder();
+                World world = Sponge.getServer().getWorlds().iterator().next();
+                ArrayList<Entity> entities = (ArrayList<Entity>) world.getNearbyEntities(new Vector3d(request.getCenter().getX(), request.getCenter().getY(), request.getCenter().getZ()), request.getRadius());
+                for (Entity entity : entities) {
+	                builder.addEntities(Minecraft.Entity.newBuilder()
+                            .setId(entity.getUniqueId().toString())
+                            .setType(Minecraft.EntityType.valueOf("ENTITY_" + entityNamesToEntityTypes.get(entity.getType().getName())))
+                            .setPosition(Point.newBuilder()
+                                            .setX((int)entity.getLocation().getX())
+                                            .setY((int)entity.getLocation().getY())
+                                            .setZ((int)entity.getLocation().getZ())
+                                            .build())
+                            .setIsLoaded(entity.isLoaded()))
+                            .build();
+                }
+                responseObserver.onNext(builder.build());
+                responseObserver.onCompleted();
+            }
+        ).name("readCube").submit(plugin);
+    }
+
+    @Override
+    public void readEntities(Uuids request, StreamObserver<Entities> responseObserver) {
+        Task.builder().execute(() -> {
+            Entities.Builder builder = Entities.newBuilder();
+            World world = Sponge.getServer().getWorlds().iterator().next();
+            for(String id : request.getUuidsList()) {
+                Optional<Entity> entityOption = world.getEntity(UUID.fromString(id));
+                if(!entityOption.isPresent()){
+                    builder.addEntities(Minecraft.Entity.newBuilder()
+                        //Proto ignores defualt values so there is no need to set type, position and isloaded
+                        .setId(id)).build();
+                } else {
+                    org.spongepowered.api.entity.Entity entity = entityOption.get();
+                    Location location = entity.getLocation();
+                    builder.addEntities(Minecraft.Entity.newBuilder()
+                        .setId(id)
+                        .setType(Minecraft.EntityType.valueOf("ENTITY_" + entityNamesToEntityTypes.get(entity.getType().getName())))
+                        .setPosition(Point.newBuilder()
+                                        .setX((int)location.getX())
+                                        .setY((int)location.getY())
+                                        .setZ((int)location.getZ())
+                                        .build())
+                        .setIsLoaded(entity.isLoaded())
+                        ).build();
+                }
+            }
+            responseObserver.onNext(builder.build());
+            responseObserver.onCompleted();
+        }).name("spawnEntities").submit(plugin);
+    }
+
+    @Override
+    public void spawnEntities(SpawnEntities request, StreamObserver<Uuids> responseObserver){
+        Task.builder().execute(() -> {
+            Uuids.Builder builder = Uuids.newBuilder();
+            World world = Sponge.getServer().getWorlds().iterator().next();
+            for (dk.itu.real.ooe.Minecraft.SpawnEntity entity : request.getSpawnEntitiesList()) {
+                try {
+                    org.spongepowered.api.entity.EntityType entityType = (org.spongepowered.api.entity.EntityType) EntityTypes.class.getField(entity.getType().toString().split("_", 2)[1]).get(null);
+                    Point pos = entity.getSpawnPosition();
+                    org.spongepowered.api.entity.Entity newEntity = world.createEntity(entityType, new Vector3d(pos.getX(), pos.getY(), pos.getZ()));
+                    try (StackFrame frame = Sponge.getCauseStackManager().pushCauseFrame()) {
+                        frame.addContext(EventContextKeys.SPAWN_TYPE, SpawnTypes.PLUGIN);
+                        world.spawnEntity(newEntity);
+                    }
+                builder.addUuids(newEntity.getUniqueId().toString()).build();
+                } catch (IllegalStateException | NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e){
+                    this.plugin.getLogger().info(e.getMessage());
+                }
+            }
+            responseObserver.onNext(builder.build());
+            responseObserver.onCompleted();
+        }).name("spawnEntities").submit(plugin);
+    }
+
+    @Override
     public void readCube(Cube cube, StreamObserver<Blocks> responseObserver) {
         Task.builder().execute(() -> {
-                    Blocks.Builder builder = Blocks.newBuilder();
-                    World world = Sponge.getServer().getWorlds().iterator().next();
-                    Point min = cube.getMin();
-                    Point max = cube.getMax();
-                    for (int x = min.getX(); x <= max.getX(); x++) {
-                        for (int y = min.getY(); y <= max.getY(); y++) {
-                            for (int z = min.getZ(); z <= max.getZ(); z++) {
-                                String name = world.getLocation(x, y, z).getBlock().getType().getName();
-                                builder.addBlocks(Block.newBuilder()
-                                        .setPosition(Point.newBuilder()
+            Blocks.Builder builder = Blocks.newBuilder();
+            World world = Sponge.getServer().getWorlds().iterator().next();
+            Pair<Vector3d, Vector3d> boundaries = findCubeBoundaries(cube.getMin(), cube.getMax());
+            Vector3d min = boundaries.getLeft();
+            Vector3d max = boundaries.getRight();
+
+            for (int x = (int)min.getX(); x <= max.getX(); x++) {
+                for (int y = (int)min.getY(); y <= max.getY(); y++) {
+                    for (int z = (int)min.getZ(); z <= max.getZ(); z++) {
+                        String name = world.getLocation(x, y, z).getBlock().getType().getName();
+                        builder.addBlocks(Block.newBuilder()
+                                .setPosition(Point.newBuilder()
                                                 .setX(x)
                                                 .setY(y)
                                                 .setZ(z)
-                                                .build()
-                                        )
-                                        .setType(Minecraft.BlockType.valueOf(blockNamesToBlockTypes.get(name))).build());
-                            }
-                        }
+                                                .build())
+                                .setType(Minecraft.BlockType.valueOf(blockNamesToBlockTypes.get(name))).build());
                     }
-                    responseObserver.onNext(builder.build());
-                    responseObserver.onCompleted();
                 }
-        ).name("readCube").submit(plugin);
+            }
+            responseObserver.onNext(builder.build());
+            responseObserver.onCompleted();
+        }).name("readCube").submit(plugin);
     }
 
     @Override
     public void fillCube(FillCubeRequest request, StreamObserver<Empty> responseObserver) {
         Task.builder().execute(() -> {
-                    World world = Sponge.getServer().getWorlds().iterator().next();
-                    Cube c = request.getCube();
-                    Point min = c.getMin();
-                    Point max = c.getMax();
-                    BlockType type;
-                    try {
-                        Field typeField = BlockTypes.class.getField(request.getType().toString());
-                        type = (BlockType) typeField.get(null);
-                    } catch (NoSuchFieldException | IllegalAccessException e) {
-                        throw new RuntimeException(e);
-                    }
+            World world = Sponge.getServer().getWorlds().iterator().next();
+            Cube c = request.getCube();
+            Pair<Vector3d, Vector3d> boundaries = findCubeBoundaries(c.getMin(), c.getMax());
+            Vector3d min = boundaries.getLeft();
+            Vector3d max = boundaries.getRight();
+            BlockType type;
+            try {
+                Field typeField = BlockTypes.class.getField(request.getType().toString());
+                type = (BlockType) typeField.get(null);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
 
-                    for (int x = min.getX(); x <= max.getX(); x++) {
-                        for (int y = min.getY(); y <= max.getY(); y++) {
-                            for (int z = min.getZ(); z <= max.getZ(); z++) {
-                                world.setBlockType(x, y, z, type);
-                            }
-                        }
+            for (int x = (int)min.getX(); x <= max.getX(); x++) {
+                for (int y = (int)min.getY(); y <= max.getY(); y++) {
+                    for (int z = (int)min.getZ(); z <= max.getZ(); z++) {
+                        world.setBlockType(x, y, z, type);
                     }
-
-                    responseObserver.onNext(Empty.getDefaultInstance());
-                    responseObserver.onCompleted();
                 }
-        ).name("fillCube").submit(plugin);
+            }
+            responseObserver.onNext(Empty.getDefaultInstance());
+            responseObserver.onCompleted();
+        }).name("fillCube").submit(plugin);
+    }
+
+    private Pair<Vector3d, Vector3d> findCubeBoundaries(Point p1, Point p2){
+        int minX = Math.min(p1.getX(), p2.getX());
+        int minY = Math.min(p1.getY(), p2.getY());
+        int minZ = Math.min(p1.getZ(), p2.getZ());
+        Vector3d min = new Vector3d(minX, minY, minZ);
+        int maxX = Math.max(p1.getX(), p2.getX());
+        int maxY = Math.max(p1.getY(), p2.getY());
+        int maxZ = Math.max(p1.getZ(), p2.getZ());
+        Vector3d max = new Vector3d(maxX, maxY, maxZ);
+
+        return new ImmutablePair<>(min, max);
     }
 
     public void setOrientation(Location<World> blockLoc, Orientation orientation, BlockType btype) throws IllegalStateException{
@@ -138,4 +245,5 @@ public class MinecraftService extends MinecraftServiceImplBase {
         }
         blockLoc.setBlock(newState.get());
     }
+
 }
